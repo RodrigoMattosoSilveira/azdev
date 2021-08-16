bb124858a180df0155b35909e261af9c57fce04a
Rodrigo Silveira <r3850365@gmail.com>
2021-08-16 09:08:56 -0700

    Implemented logic to use Promise to resolve a GraphQL request that resolves asynchronously

--- api/src/schema/index.js
+++ api/src/schema/index.js
@@ -14,11 +14,22 @@ import {numbersInRangeAverageObject, numbersInRangeObject} from '../utils';
 const QueryType = new GraphQLObjectType({
     name: 'Query',
     fields: {
+        // currentTime: {
+        //     type: GraphQLString,
+        //     resolve: () => {
+        //         const isoString = new Date().toISOString();
+        //         return isoString.slice(11, 19);
+        //     },
+        // },
         currentTime: {
             type: GraphQLString,
             resolve: () => {
-                const isoString = new Date().toISOString();
-                return isoString.slice(11, 19);
+                return new Promise(resolve => {
+                    setTimeout(() => {
+                        const isoString = new Date().toISOString();
+                        resolve(isoString.slice(11, 19));
+                    }, 5000);
+                });
             },
         },
         sumNumbersInRange: {
