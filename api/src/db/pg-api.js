ba2fedc895eddac6ddb9424bf1917112b7317458
Rodrigo Silveira <r3850365@gmail.com>
2021-08-18 11:23:22 -0700

    Implemented logic to memoize the User record for a given transaction;

--- api/src/db/pg-api.js
+++ api/src/db/pg-api.js
@@ -15,6 +15,12 @@ const pgApiWrapper = async () => {
             const pgResp = await pgQuery(sqls.usersFromIds, {$1: [userId]}); // Passes $1 to the SQL statement as [userId]
             return pgResp.rows[0];
         },
+        usersInfo: async (userIds) => {
+            const pgResp = await pgQuery(sqls.usersFromIds, {$1: [userIds]}); // Passes $1 to the SQL statement as [userIds]
+            return userIds.map((userId) =>
+                pgResp.rows.find((row) => userId === row.id) // Uses a .map call on the input array to ensure that the output array has the exact same length and order. DataLoader will not work properly if you don’t do that.
+            );
+        },
         approachList: async (taskId) => {
             const pgResp = await pgQuery(sqls.approachesForTaskIds, {
                 $1: [taskId], // Passes $1 to the SQL statement as [taskId]
--- api/src/server.js
+++ api/src/server.js
@@ -1,13 +1,13 @@
 /** GIA NOTES
  *
  * Use the code below to start a bare-bone express web server
-
  */
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import morgan from 'morgan';
 import { graphqlHTTP } from 'express-graphql';
+import DataLoader from 'dataloader';
 import { schema } from './schema';
 
 import * as config from './config';
@@ -24,24 +24,28 @@ async function main() {
   server.use('/:fav.ico', (req, res) => res.sendStatus(204));
 
   // Example route
-  server.use('/',    graphqlHTTP({
-    schema,
-    context: { pgApi },
-    graphiql: true,
-    customFormatErrorFn: (err) => {
-      const errorReport = {
-        message: err.message,
-        locations: err.locations,
-        stack: err.stack ? err.stack.split('\n') : [], // Makes the error stack show up in development, which is very handy
-          path: err.path,
+  server.use('/', (req, res) => {
+    const loaders = {
+      users: new DataLoader((userIds) => pgApi.usersInfo(userIds)),
     };
-      console.error('GraphQL Error', errorReport); // Logs the error in the server logs
+    graphqlHTTP({
+      schema,
+      context: { loaders, pgApi },
+      graphiql: true,
+      customFormatErrorFn: (err) => {
+        const errorReport = {
+          message: err.message,
+          locations: err.locations,
+          stack: err.stack ? err.stack.split('\n') : [], // Makes the error stack show up in development, which is very handy
+          path: err.path,
+        };
+        console.error('GraphQL Error', errorReport); // Logs the error in the server logs
         return config.isDev
-            ? errorReport
-            : { message: 'Oops! Something went wrong! :(' }; // Returns a generic error in production
-    },
-
-  }));
+          ? errorReport
+          : {message: 'Oops! Something went wrong! :('}; // Returns a generic error in production
+        },
+      })(req, res);
+  });
 
   // This line rus the server
   server.listen(config.port, () => {
